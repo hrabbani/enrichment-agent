467fdedd4c92cde0c526b7255a889f004b100284
hrabbani <humza.rabbani@gmail.com>
2025-05-22 04:45:45 -0500

    configured SQS, added basic enrichment route

--- server/routes/leads.ts
+++ server/routes/leads.ts
@@ -1,5 +1,10 @@
 import { Router, Request, Response } from "express";
 import { getAllLeads, getLeadByID, createLead } from "../queries/leads";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const router = Router();
 
@@ -11,6 +16,30 @@ router.get("/", async (req: Request, res: Response) => {
 router.get("/:id", async (req: Request, res: Response) => {
   const lead = await getLeadByID(req.params.id);
   res.json(lead);
+});
+
+router.post("/:id/enrich", async (req: Request, res: Response) => {
+  const enrichment_client = new SQSClient({});
+  const ENRICHMENT_QUEUE_URL = process.env.enrichment_queue_url;
+  console.log("Enrichment queue URL:", ENRICHMENT_QUEUE_URL);
+
+  const enrichment_params = {
+    MessageBody: JSON.stringify({
+      leadId: req.params.id,
+    }),
+    QueueUrl: ENRICHMENT_QUEUE_URL,
+  };
+
+  try {
+    const enrichment_result = await enrichment_client.send(
+      new SendMessageCommand(enrichment_params)
+    );
+    res.status(201).json(enrichment_result);
+    console.log("SQS response:", enrichment_result);
+  } catch (err) {
+    res.status(500).json(enrichment_params);
+    console.log("Error sending message", err);
+  }
 });
 
 router.post("/", async (req: Request, res: Response) => {
